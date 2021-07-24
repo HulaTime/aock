b838fc852f586803470a5d47c8e873f757eb64dc
Author Name <hulatime@tutanota.com>
2021-07-24 16:16:19 +0100

    evaluate if headers were included on the request

--- lib/RequestTest.ts
+++ lib/RequestTest.ts
@@ -1,12 +1,5 @@
 
-import { IncomingMessage, ServerResponse } from 'http';
-
-// interface SObject {
-//   [key: string]: string;
-// }
-// interface TestCondition {
-//   (): boolean;
-// }
+import { IncomingHttpHeaders, IncomingMessage, ServerResponse } from 'http';
 
 enum Methods {
   GET = 'get',
@@ -24,22 +17,34 @@ interface TestEvaluation {
   pathMatchedOnCall?: number
   methodMatched: boolean
   methodMatchedOnCall?: number
+  headersMatched?: boolean
 }
+
+type StringObject = Record<string, string>;
 
 class RequestTest {
   private called = false;
   private _callCount = 0;
   private _method = Methods.GET;
   private _path = '/';
-  // private _headers?: SObject;
-  // private _query?: SObject;
-  // private _testConditions?: TestCondition[];
+  private _headers?: IncomingHttpHeaders;
 
-  private _isSatisfied = false;
+  private _matchHeaders?: Record<string, string>;
+
+  // private _isSatisfied = false;
   private _methodMatched = false;
   private _pathMatched = false;
   private _methodMatchedOnCall?: number;
   private _pathMatchedOnCall?: number;
+
+  private _lowerCaseObjectKeys(input: StringObject): StringObject {
+    const inputKeys = Object.keys(input);
+    const lowerCaseReducer = (acc: StringObject, inputKey: string): StringObject => {
+      acc[inputKey.toLowerCase()] = input[inputKey];
+      return acc;
+    };
+    return inputKeys.reduce(lowerCaseReducer, {});
+  }
 
   private _setPath(path: string) {
     if (path[0] !== '/') {
@@ -77,6 +82,11 @@ class RequestTest {
     return this;
   }
 
+  public headers(matchHeaders: Record<string, string>): RequestTest {
+    this._matchHeaders = this._lowerCaseObjectKeys(matchHeaders);
+    return this;
+  }
+
   private _handleMethodMatch() {
     this._methodMatched = true;
     this._methodMatchedOnCall = this.getCallCount() || undefined;
@@ -85,6 +95,34 @@ class RequestTest {
   private _handlePathMatch() {
     this._pathMatched = true;
     this._pathMatchedOnCall = this.getCallCount() || undefined;
+  }
+
+  private _reqIncludesTestHeaders(): boolean {
+    if (!this._matchHeaders) {
+      // No headers to match against so return true
+      return true;
+    }
+    if (!this._headers || Object.keys(this._headers).length === 0) {
+      // If there are no headers in the request to match against but 
+      // test headers are defined, then must return false
+      return false;
+    }
+
+    // Check if the req headers include all the the test headers, 
+    // if not return false
+    const testHeaderKeys = Object.keys(this._matchHeaders);
+    for (let i = 0; i < testHeaderKeys.length; i++) {
+      if (!this._headers[testHeaderKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private _getSatisfaction(): boolean {
+    return this._pathMatched
+    && this._methodMatched
+    && this._reqIncludesTestHeaders();
   }
 
   private _called() {
@@ -98,13 +136,14 @@ class RequestTest {
     return (req: IncomingMessage, res: ServerResponse): void => {
       res;
 
-      const { method, url } = req;
-      if (method === this._method) {
-        this._handleMethodMatch();
-        if (url === this._path) {
-          this._handlePathMatch();
+      const { method, url, headers } = req;
+      if (url === this._path) {
+        this._handlePathMatch();
+        if (method === this._method) {
+          this._handleMethodMatch();
+          this._headers = this._lowerCaseObjectKeys(headers as StringObject);
+          this._called();
         }
-        this._called();
       }
     };
   }
@@ -114,15 +153,13 @@ class RequestTest {
   }
 
   public evaluate(): TestEvaluation {
-    if (this._pathMatched && this._methodMatched) {
-      this._isSatisfied = true;
-    }
     return {
-      isSatisfied: this._isSatisfied,
+      isSatisfied: this._getSatisfaction(),
       methodMatched: this._methodMatched,
       methodMatchedOnCall: this._methodMatchedOnCall,
       pathMatched: this._pathMatched,
       pathMatchedOnCall: this._pathMatchedOnCall,
+      headersMatched: this._reqIncludesTestHeaders(),
     };
   }
 }
--- tests/ReqTest.test.ts
+++ tests/ReqTest.test.ts
@@ -1,8 +1,19 @@
-// jest.mock('http');
-
 import { IncomingMessage, ServerResponse } from 'http';
 
-import RequestTest from '../lib/RequestTest';
+import RequestTest, { TestListener } from '../lib/RequestTest';
+
+const uriUnderTest = '/foo/bar';
+const headersUnderTest = {
+  'authorization': 'Bearer 123',
+  'content-type': 'application/json',
+};
+
+const listenerRequest = {
+  method: 'get',
+  url: uriUnderTest,
+  headers: headersUnderTest,
+} as IncomingMessage;
+const listenerResponse = {} as ServerResponse;
 
 describe('ReqTest class', () => {
   test('it should exist', () => {
@@ -15,10 +26,21 @@ describe('ReqTest class', () => {
 
   describe('class instance', () => {
     let reqTest: RequestTest;
-    // const listenerResponse = {} as ServerResponse;
 
     beforeEach(() => {
       reqTest = new RequestTest();
+    });
+
+    test('it should have a function that returns the number of times the test has been triggered', () => {
+      expect(reqTest.getCallCount()).toBe(0);
+    });
+
+    test('should have a method getCallCount', () => {
+      expect(typeof reqTest.getCallCount).toBe('function');
+    });
+
+    test('#getCallCount should initially return 0', () => {
+      expect(reqTest.getCallCount()).toBe(0);
     });
 
     describe('Setting test conditions', () => {
@@ -30,14 +52,20 @@ describe('ReqTest class', () => {
         expect(typeof reqTest.delete).toBe('function');
       });
 
-      test('#get takes a single path parameter which is the path under test', () => {
-        reqTest.get('/path');
+      test('#get takes a single path parameter which is the path under test and returns the class instance', () => {
+        const res = reqTest.get('/path');
+        expect(res).toBe(reqTest);
       });
 
       test('#get the path paramteter must start with a "/"', () => {
         expect(() => {
           reqTest.get('path');
         }).toThrow('"path" must be prefixed with "/"');
+      });
+
+      test('#headers sets the expected headers and returns the class instance', () => {
+        const res = reqTest.headers({});
+        expect(res).toBe(reqTest);
       });
     });
 
@@ -46,14 +74,32 @@ describe('ReqTest class', () => {
         const listener = reqTest.createListener();
         expect(typeof listener).toBe('function');
       });
+
+      test('if method and path have been set, calling the listener with a matching request increments the call count', () => {
+        reqTest.get(uriUnderTest);
+        const listener = reqTest.createListener();
+        expect(reqTest.getCallCount()).toBe(0);
+        listener(listenerRequest, listenerResponse);
+        expect(reqTest.getCallCount()).toBe(1);
+        listener(listenerRequest, listenerResponse);
+        expect(reqTest.getCallCount()).toBe(2);
+      });
+
+      test('if method and path have been set, calling the listener with a request that does not match, does not increment the call count', () => {
+        reqTest.get('/bar/foo');
+        const listener = reqTest.createListener();
+        expect(reqTest.getCallCount()).toBe(0);
+        listener(listenerRequest, listenerResponse);
+        expect(reqTest.getCallCount()).toBe(0);
+      });
     });
 
     describe('Evaluate test conditions', () => {
-      const listenerRequest = {
-        method: 'get',
-        url: '/abc'
-      } as IncomingMessage;
-      const listenerResponse = {} as ServerResponse;
+      let listener: TestListener;
+
+      beforeEach(() => {
+        listener = reqTest.createListener();
+      });
 
       test('there is a method #evaluate', () => {
         expect(typeof reqTest.evaluate).toBe('function');
@@ -69,53 +115,60 @@ describe('ReqTest class', () => {
         expect(isSatisfied).toBe(false);
       });
 
-      describe('HTTP verb and path matched', () => {
-        describe('GET', () => {
-          test('if path is matched, #evaluate should be satisfied if the http verb is the only test condition', () => {
-            const listener = reqTest.createListener();
-            reqTest.get('/abc');
-            listener(listenerRequest, listenerResponse);
-            const { isSatisfied } = reqTest.evaluate();
-            expect(isSatisfied).toBe(true);
-          });
+      describe('Evaluate Get request', () => {
+        test('if path is matched, #evaluate should be satisfied if the http verb is the only test condition', () => {
+          reqTest.get(uriUnderTest);
+          listener(listenerRequest, listenerResponse);
+          const { isSatisfied } = reqTest.evaluate();
+          expect(isSatisfied).toBe(true);
+        });
 
-          test('if path is matched, #evaluate should inform the path was matched', () => {
-            const listener = reqTest.createListener();
-            reqTest.get('/abc');
-            listener(listenerRequest, listenerResponse);
-            const { pathMatched } = reqTest.evaluate();
-            expect(pathMatched).toBe(true);
-          });
+        test('if path is matched, #evaluate should inform the path was matched', () => {
+          reqTest.get(uriUnderTest);
+          listener(listenerRequest, listenerResponse);
+          const { pathMatched } = reqTest.evaluate();
+          expect(pathMatched).toBe(true);
+        });
+
+        test('If path is not matched for the request, can\'t get no satisfaction', () => {
+          reqTest.get('/i/cant/get/no');
+          listener(listenerRequest, listenerResponse);
+          const { isSatisfied: satisfaction } = reqTest.evaluate();
+          expect(satisfaction).toBe(false);
+        });
+
+        test('If path is not matched for the request, #evaluate should return pathMatched false', () => {
+          reqTest.get('/i/cant/get/no');
+          listener(listenerRequest, listenerResponse);
+          const { pathMatched } = reqTest.evaluate();
+          expect(pathMatched).toBe(false);
+        });
+      });
+
+      describe('Evaluate Get request with headers', () => {
+        test('when all conditions are satisfied', () => {
+          reqTest
+            .get(uriUnderTest)
+            .headers({ 'Content-Type': headersUnderTest['content-type'] });
+          listener(listenerRequest, listenerResponse);
+          const { isSatisfied, headersMatched } = reqTest.evaluate();
+          expect(headersMatched).toBe(true);
+          expect(isSatisfied).toBe(true);
+        });
+
+        test('when not all test headers are present in the request', () => {
+          reqTest
+            .get(uriUnderTest)
+            .headers({
+              'Content-Type': headersUnderTest['content-type'],
+              'x-correlation-id': '07ad9596-1019-4dad-bbed-17c1075e33b7'
+            });
+          listener(listenerRequest, listenerResponse);
+          const { isSatisfied, headersMatched } = reqTest.evaluate();
+          expect(headersMatched).toBe(false);
+          expect(isSatisfied).toBe(false);
         });
       });
     });
-
-    // test('it should have a function that returns the number of times the test has been triggered', () => {
-    //   expect(reqTest.getCallCount()).toBe(0);
-    // });
-
-    // test('should have a method getCallCount', () => {
-    //   expect(typeof reqTest.getCallCount).toBe('function');
-    // });
-
-    // test('#getCallCount should initially return 0', () => {
-    //   expect(reqTest.getCallCount()).toBe(0);
-    // });
-
-
-    // describe('#createListener', () => {
-    //   test('returns a listener', () => {
-    //     expect(typeof reqTest.createListener()).toBe('function');
-    //   });
-
-    //   test('calling the listener increments the class call count', () => {
-    //     const listener = reqTest.createListener();
-    //     expect(reqTest.getCallCount()).toBe(0);
-    //     listener(listenerRequest, listenerResponse);
-    //     expect(reqTest.getCallCount()).toBe(1);
-    //     listener(listenerRequest, listenerResponse);
-    //     expect(reqTest.getCallCount()).toBe(2);
-    //   });
-    // });
   });
 });
